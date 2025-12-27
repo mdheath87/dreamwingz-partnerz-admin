8b9fe2cd0d47d6bfb98770ec2a2508c71c3deb09
Matthew Burns <matthewb@dreamwingz.net>
2025-12-26 18:25:45 -0600

    Add pages router homepage

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Dream Wingz Partners</h1>
+      <p>Partner employee discount portal</p>
+      <p style={{ marginTop: 16, color: "#555" }}>
+        Admin portal coming soon.
+      </p>
+    </main>
+  );
+}
